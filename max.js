29ac1cb679fbe5a64034fcdd9ff76b0caeec1d05
Sumaiya Akter Shukhy <84353537+shukhy@users.noreply.github.com>
2023-06-24 23:44:27 +0600

    Add files via upload

---
+++ max.js
@@ -0,0 +1,20 @@
+var tuly = 900;
+var sukhy =5000;
+var riad = 800;
+
+if(tuly>sukhy){
+    if(tuly>riad){
+        console.log("tuly is get bigger amount of money");
+    }
+    else{
+        console.log("riad is get bigger amount of money"); 
+    }
+}
+else{
+    if(sukhy>riad){
+        console.log("sukhy is get bigger amount of money");
+    }
+    else{
+        console.log("riad is get bigger amount of money");
+    }
+}
